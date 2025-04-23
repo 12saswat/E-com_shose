de5e20308230e2723ecc51535be0ebdea285b4df
saswatjit ptra <saswatp813@gmail.com>
2025-04-23 23:57:48 +0530

    correcting the ratingstar file to identify the number or not

--- src/components/Sections/Products/RatingStars.tsx
+++ src/components/Sections/Products/RatingStars.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+type RatingStarsProps = {
+  rating: number;
+};
 
-const RatingStars = ({ rating }) => {
+const RatingStars = ({ rating }: RatingStarsProps) => {
   const totalStars = 5;
   return (
     <span className="text-orange-500 text-[19px] h-0 leading-none">
